2d776fba5e037bca45c02a8334c5f8f35693d980
yangxuserene <yangxuserene@gmail.com>
2015-07-27 13:41:12 -0500

    first commit

---
+++ listgen.py
@@ -0,0 +1,97 @@
+import sys
+alloc_file = 'allocation.conf'
+
+def contiguous_alloc(job_ranks, total_nodes):
+    f = open(alloc_file,'w')
+    start=0
+    for num_rank in range(len(job_ranks)):
+        for rankid in range(start, start+job_ranks[num_rank]):
+            f.write("%s " % rankid)
+        f.write("\n")
+        start += job_ranks[num_rank]
+    f.closed
+
+
+
+def stripe_alloc(job_ranks, total_nodes):
+    print "the num of nodes of each Job", job_ranks 
+    f = open(alloc_file,'w')
+    node_list = range(0, int(total_nodes))
+    stripe_size = 10
+    alloc_list = []
+    for num_rank in range(len(job_ranks)):
+        print "job id", num_rank
+        num_stripe = 1
+        start = num_rank*stripe_size
+        if(job_ranks[num_rank] % stripe_size != 0):
+            num_stripe = job_ranks[num_rank]/stripe_size+1
+        else:
+            num_stripe = job_ranks[num_rank]/stripe_size
+        tmp_list = []
+        while(num_stripe>0):
+            tmp_list += node_list[start:start+stripe_size]
+            start += len(job_ranks)*stripe_size
+            num_stripe -= 1
+        alloc_list.append(tmp_list) 
+    print "the allocation list of all job: \n", alloc_list
+    
+    for job_id in range (len(alloc_list)):
+        tmp = alloc_list[job_id]
+        print "alloc list for JOB", job_id
+        for rankid in range (job_ranks[job_id]):
+           # print tmp[rankid]
+            f.write("%s " % tmp[rankid])
+        f.write("\n")
+    f.closed
+
+
+
+def chunk_alloc(job_ranks, total_nodes): 
+    f = open(alloc_file, 'w')
+    start = 0
+    for num_rank in range(len(job_ranks)):
+        chunk_size = 10
+        num_chunk = 1
+        required_size = 10
+        while(job_ranks[num_rank] > required_size):
+            num_chunk += 1
+            required_size = chunk_size * num_chunk
+
+        #print job_ranks[num_rank], "========", required_size, num_chunk
+        for rankid in range(start, start+job_ranks[num_rank]):
+            f.write("%s " % rankid)
+        f.write("\n")
+        start += required_size 
+    f.closed
+
+
+def policy_select(plcy, job_ranks, total_nodes):
+    if plcy == "CONT":
+        print "contiguous alloction!"
+        contiguous_alloc(job_ranks,  total_nodes)
+    elif plcy == "CHUNK":
+        print "chunk allocation!"
+        chunk_alloc(job_ranks, total_nodes)
+    elif plcy == "STRIPE":
+        print "stripe allcation!"
+        stripe_alloc(job_ranks, total_nodes)
+    else:
+        print "NOT Supported yet!"
+
+
+if __name__ == "__main__":
+    f = open(sys.argv[1], "r")
+    array = []
+
+    for line in f:
+        for number in line.split():
+            array.append(number); 
+
+    f.close()
+    alloc_plcy = array.pop(0)
+    total_nodes = array.pop(0)
+    print alloc_plcy
+    array = map(int, array)
+    print array
+    policy_select(alloc_plcy, array, total_nodes)
+
