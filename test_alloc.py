774a2476571f05a96a02120cbe82ee5722899dab
yangxuserene <yangxuserene@gmail.com>
2016-06-06 11:27:35 -0500

    add three basic alloc method for torus network

--- test_alloc.py
+++ test_alloc.py
@@ -6,5 +6,11 @@ from dragonfly import Dragonfly
 #  Dragonfly(8, 33, 'rand_router', jobrank_list, 1)
 #  Dragonfly(8, 33, 'rand_group', jobrank_list, 1)
 #  Dragonfly(8, 33, 'rand_node', jobrank_list, 1)
-Dragonfly(8, 33, 'hyb', jobrank_list, 3)
+#  Dragonfly(8, 33, 'hyb', jobrank_list, 3)
+
+jobrank_list = [10, 5, 10]
+torus_dim = [8, 16, 16]
+Torus(torus_dim, 'cont', jobrank_list, 3)
+Torus(torus_dim, 'rand', jobrank_list, 3)
+Torus(torus_dim, 'hyb', jobrank_list, 3)
 
